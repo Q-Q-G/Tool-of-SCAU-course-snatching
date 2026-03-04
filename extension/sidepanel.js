1b884ee12301f1ad2f19f6dce338b01f456fb204
SQG1G <134822000+Q-Q-G@users.noreply.github.com>
2026-03-04 22:35:37 +0800

    Add files via upload

---
+++ extension/sidepanel.js
@@ -0,0 +1,23 @@
+const GOOGLE_ORIGIN = "https://jwzf.scau.edu.cn";
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  if (!tab.url) return;
+  const url = new URL(tab.url);
+  if (url.origin === GOOGLE_ORIGIN) {
+    await chrome.sidePanel.setOptions({
+      tabId,
+      path: "sidepanel.html",
+      enabled: true,
+    });
+  } else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId,
+      enabled: false,
+    });
+  }
+});
+
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
